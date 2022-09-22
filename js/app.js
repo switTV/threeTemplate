4f1c675e4e0b59822da6522db12b526ff6663703
swittv <4galarzajuan2004@gmail.com>
2022-09-22 14:34:22 -0300

    agregar archivos importantes

---
+++ js/app.js
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+renderer.setClearColor(0xff3ee)
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(-90, 140, 140);
+orbit.update();
+
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
+
+function animate () {
+    renderer.render(scene, camera)
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
